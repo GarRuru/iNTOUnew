b7a9e68dfbf79f825be1dc5ca05e9cd03977dcec
GarRuru <nick92251@gmail.com>
2017-08-29 10:25:44 +0800

    Initial Commit

---
+++ iNTOUnew/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iNTOUnew
+//
+//  Created by Yu-Hsuan Chen on 29/08/2017.
+//  Copyright © 2017 Yu-Hsuan Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
